20e441fb5a985be050494cea5415bb6223bcb16a
SandroNeumann <sandro.neumann@idgis.nl>
2025-05-09 11:52:17 +0200

    Add max tries mechanism to wfs health check

--- scripts/update-bedrijventerreinen.sh
+++ scripts/update-bedrijventerreinen.sh
@@ -3,9 +3,18 @@
 # Load environment variables
 . /etc/gemeente
 
+# 15 minutes in terms of sleep between tries
+MAX_TRIES=90
+try=1
 until curl -sf "$BEDRIJVENTERREINEN_FEATURES"; do
-  echo "Waiting for Bedrijventerreinen WFS..."
-  sleep 5
+  echo "Waiting for Bedrijventerreinen WFS, try $try"
+  if [ "$try" -gt "$MAX_TRIES" ]; then
+    echo "max tries reached, exiting"
+    exit 1
+  fi
+
+  try=$((try +1))
+  sleep 10
 done
 
 echo "Bedrijventerreinen WFS is ready"
