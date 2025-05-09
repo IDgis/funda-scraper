0814cdfb02d5a7d672fd75bd8e644295eef5d274
SandroNeumann <sandro.neumann@idgis.nl>
2025-05-09 09:16:31 +0200

    Add wfs health check to update bedrijven terreinen script

--- scripts/update-bedrijventerreinen.sh
+++ scripts/update-bedrijventerreinen.sh
@@ -3,5 +3,12 @@
 # Load environment variables
 . /etc/gemeente
 
+until curl -sf "$BEDRIJVENTERREINEN_FEATURES"; do
+  echo "Waiting for Bedrijventerreinen WFS..."
+  sleep 5
+done
+
+echo "Bedrijventerreinen WFS is ready"
+
 # Download new bedrijventerreinen features
 curl --insecure "$BEDRIJVENTERREINEN_FEATURES" > /tmp/bedrijventerreinen.json
